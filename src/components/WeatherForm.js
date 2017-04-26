db8c435d7bea2bef8cea7f6a111728999d175ba8
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-26 18:44:36 +0700

    ket thuc

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 export default class WeatherForm extends Component {
     constructor(props) {
@@ -9,24 +9,37 @@ export default class WeatherForm extends Component {
 
     changeCityName() {
         const { city } = this.state;
-        const { onChangeTemp, onToggle } = this.props;
+        const { onChangeTemp, onToggle, onChangeCity } = this.props;
         onToggle();
-        this.props.onChangeCity(city);
         this.setState({ city: '' });
         const api = '01cc37655736835b0b75f2b395737694';
         const url = `http://api.openweathermap.org/data/2.5/find?units=metric&appid=${api}&q=${city}`;
         fetch(url)// eslint-disable-line
-        .then(res => res.json())
-        .then(resJSON => {
-            onChangeTemp(resJSON.list[0].main.temp);
-            onToggle();
-        });
+            .then(res => res.json())
+            .then(resJSON => {
+                onToggle();
+                console.log(resJSON);
+                if (resJSON.count > 0) {
+                    onChangeCity(city);
+                    onChangeTemp(resJSON.list[0].main.temp);
+                } else {
+                    Alert.alert(
+                        'Error 404',
+                        'Cannot find city',
+                        [
+                            { text: 'OK', onPress: () => console.log('Ask me later pressed') }
+                        ],
+                        { cancelable: false }
+                    );
+                }
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
             <View style={{ backgroundColor: '#B7B7B7', alignItems: 'center' }}>
-                <TextInput 
+                <TextInput
                     style={{ width: 300, height: 40, backgroundColor: '#fff', paddingLeft: 10 }}
                     value={this.state.city}
                     onChangeText={text => this.setState({ city: text })}
